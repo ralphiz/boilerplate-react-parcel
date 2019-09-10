40c9def00e3d84dd797b71efbaba79e9cef3bf63
Ralph Cacho <cacho.rc@gmail.com>
2019-09-09 22:15:30 -0700

    Update packages, create card component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,12 @@
 import React from "react";
+import Card from "./Card";
 
 const Home = () => {
   return (
-    <div className="container mx-auto">
-      <h1>This is the Home component.</h1>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img
-          className="w-full"
-          src="https://tailwindcss.com/img/card-top.jpg"
-          alt="Sunset in the mountains"
-        />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-          <p className="text-grey-darker text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
-          </p>
-        </div>
-        <div className="px-6 py-4">
-          <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">
-            #photography
-          </span>
-          <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">
-            #travel
-          </span>
-          <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">
-            #winter
-          </span>
-        </div>
+    <div className="container mx-auto py-16">
+      <div className="flex flex-column content-center">
+        <h1>This is the Home component.</h1>
+        <Card />
       </div>
     </div>
   );
